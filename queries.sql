7ee3f292c98d930b404276558a9480b32e549e8b
Nikhil Kumar Tripathy <162478137+nktri@users.noreply.github.com>
2026-04-06 02:02:04 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,14 @@
+SELECT DISTINCT event_type FROM ecommerce;
 
+SELECT event_type, COUNT(*) 
+FROM ecommerce 
+GROUP BY event_type;
+
+SELECT COUNT(DISTINCT user_id) 
+FROM ecommerce;
+
+SELECT 
+    COUNT(DISTINCT CASE WHEN event_type = 'view' THEN user_id END) as view_users,
+    COUNT(DISTINCT CASE WHEN event_type = 'cart' THEN user_id END) as cart_users,
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) as purchase_users
+FROM ecommerce;
